7b3bec6e7a683d181a6766a4c0dc1222c2958750
Dev <devsapariya94@gmail.com>
2023-09-03 16:37:23 +0530

    add CORS

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ from pymongo import MongoClient
 from flask_limiter import Limiter
 import openai
 import json  
+from flask_cors import CORS
 
 app = Flask(__name__)
 limiter = Limiter(app)
 
 
-open_ai_api = "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" # expired
+open_ai_api = "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" # expired
 mongoURL = "mongodb+srv://tubelearn:1234@cluster0.s19nica.mongodb.net/?retryWrites=true&w=majority" #currently filled with junk
 client = MongoClient(mongoURL)
 db = client["HackTheClassRoom"]
 users_collection = db["users"]
 courses_collection = db["courses"]
 
-
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 app.config['SECRET_KEY'] = 'your-secret-key'
 jwt = JWTManager(app)     
 
