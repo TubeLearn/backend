4d9a53e996a0d9456cb9bc984b16939238220d70
kevohm <kevinkbetQgmail.com>
2023-09-21 22:34:04 +0300

    [FIX]: added the titles and length to each video and also combined video-info and video route

--- app.py
+++ app.py
@@ -66,28 +66,30 @@ def login():
     return jsonify({'message': 'Invalid credentials'}), 401
 
 
-@app.route('/coures/<course_id>/video', methods=['GET'])
+@app.route('/courses/<course_id>/video', methods=['GET'])
 def get_video(course_id):
     from bson.objectid import ObjectId
     course_id = ObjectId(course_id)
 
     course = courses_collection.find_one({'_id': course_id})
     if course:
+        video_links = []
         link = course['link']
         playlist = Playlist(link)
-        video_links = list(playlist.video_urls)
-        print(video_links)
+        v = playlist.videos
+        
+        for video in v:
+            title = video.title
+            desc = video.description
+            thumbnail = video.thumbnail_url
+            length = video.length
+            data = {"title":title,"description":desc,"thumbnail":thumbnail,"length":length}
+            video_links.append(data)
         return jsonify({'video_links': video_links})
  
 
     return jsonify({'message': 'Course not found'}), 404
-
-
-@app.route('/videoinfo/<url>', methods=['GET'])
-def get_video_info(url):
-    from pytube import YouTube
-    yt = YouTube(url)
-    return jsonify({'title': yt.title, 'description': yt.description})
+    
 
 
 @app.route('/user/current', methods=['GET'])
@@ -139,8 +141,21 @@ def get_course(course_id):
 
         if course:
             return jsonify(course)
-        
+            
         return jsonify({'message': 'Course not found'}), 404
+
+    except Exception as e:
+        return jsonify({'message': 'Invalid course ID format'}), 400
+
+@app.route('/courses/<course_id>', methods=['DELETE'])
+def delete_course(course_id):
+    from bson.objectid import ObjectId
+
+    try:
+        # Convert the course_id from the URL to ObjectId
+        course_id = ObjectId(course_id)
+        courses_collection.delete_one({'_id': course_id})
+        return jsonify({'message': 'Course deleted'}), 200
 
     except Exception as e:
         return jsonify({'message': 'Invalid course ID format'}), 400
@@ -148,21 +163,36 @@ def get_course(course_id):
 @app.route('/course/add', methods=['POST'])
 def add_course():
     data = request.get_json()
-    
+    url = ""
+    try:
+        url = data['link']
+    except KeyError:
+        return jsonify({'message': 'No link provided'}), 400
     # Generate a unique course ID (e.g., using ObjectId from pymongo)
     from bson.objectid import ObjectId
     course_id = ObjectId()
-    
-    # Add a new course to MongoDB with the generated course_id
+    p = Playlist(url)
+    # Add a new course to MongoDB with the generated 
+    title = ""
+    desc = ""
+    try:
+        title = p.title
+    except:
+        title = ""
+    try:
+        desc = p.description
+    except:
+        desc = ""
+
     new_course = {
         '_id': course_id,
-        'title': data['title'],
-        'description': data['description'],
-        'link': data['link']
+        'title': title ,
+        'description': desc,
+        'link': url
     }
 
     # Check if a course with the same title already exists
-    existing_course = courses_collection.find_one({'title': data['title']})
+    existing_course = courses_collection.find_one({'title': new_course['title']})
     if existing_course:
         existing_course_id = existing_course['_id']
         return jsonify({'message': 'Course with the same title already exists', 'course_id': str(existing_course_id)}), 400
